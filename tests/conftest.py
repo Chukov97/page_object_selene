51681b02179067244286b0d2a78134fd23a7aba9
chukov97 <ichukov@licard.com>
2023-12-14 09:31:01 +0300

    fix browser size

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,8 +10,8 @@ def browser_management():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
-    browser.config.window_height = 1920
-    browser.config.window_width = 1080
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
 
     yield
 
