183938d04f78356aa0b5339efe01c9291755d182
Ilya Chukov <ilyachuk@gmail.com>
2023-12-22 01:38:05 +0300

    fixed conftest.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 from pydantic_settings import BaseSettings
 from demoqa_tests.models.constatn import BASE_URL
 from utils import path
@@ -13,6 +13,8 @@ class Config(BaseSettings):
     window_width: int = 1920
     window_height: int = 1080
     timeout: float = 4.0
+
+    remote_url: Optional[str] = None
 
 
 config = Config(_env_file=path.relative_from_root(f'.env.{Config().context}'))
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,68 @@
 import pytest
-from selene import browser
+from selene import browser, Browser, Config
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 from settings import config
 
 
 @pytest.fixture()
 def browser_management():
     browser.config.base_url = config.base_url
-    browser.config.driver_name = config.driver_name
-    browser.config.window_height = config.window_height
-    browser.config.window_width = config.window_width
     browser.config.timeout = config.timeout
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = config.load_strategy
-    browser.config.driver_options = driver_options
+    if config.remote_url:
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "100.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": False
+            }
+        }
 
-    yield
+        options.capabilities.update(selenoid_capabilities)
+        driver = webdriver.Remote(
+            command_executor=config.remote_url,
+            options=options
+        )
+
+        browser_selenoid = Browser(Config(driver))
+        yield browser_selenoid
+
+        browser.quit()
+    else:
+        browser.config.driver_name = config.driver_name
+        browser.config.window_height = config.window_height
+        browser.config.window_width = config.window_width
+        driver_options = webdriver.ChromeOptions()
+        driver_options.page_load_strategy = config.load_strategy
+        browser.config.driver_options = driver_options
+
+        yield
+
+        browser.quit()
+
+
+@pytest.fixture()
+def selenoid_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
 
     browser.quit()
