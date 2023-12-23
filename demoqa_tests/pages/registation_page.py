f380c8671c1116e9027943c44650238350ce77b5
Ilya Chukov <ilyachuk@gmail.com>
2023-12-23 18:29:22 +0300

    fixed allure steps

--- demoqa_tests/pages/registation_page.py
+++ demoqa_tests/pages/registation_page.py
@@ -1,4 +1,3 @@
-import allure
 from selene import browser, have, be, by
 from demoqa_tests.resource import path
 from demoqa_tests.enums.gender import Gender
@@ -6,77 +5,71 @@ from demoqa_tests.enums.hobby import Hobby
 from demoqa_tests.models.constatn import PRACTICE_FORM, OPEN_PAGE_URL
 
 
-@allure.title("Successful fill form")
 class RegistrationPage:
-    with allure.step("Open registrations form"):
-        def open_page(self):
-            browser.open(OPEN_PAGE_URL)
-            browser.element('//div[@class="main-header"]').should(have.text(PRACTICE_FORM))
-            return self
+    def open_page(self):
+        browser.open(OPEN_PAGE_URL)
+        browser.element('//div[@class="main-header"]').should(have.text(PRACTICE_FORM))
+        return self
 
-    with allure.step("Fill form"):
-        def enter_user_details(self, user):
-            browser.element('//input[@id="firstName"]').should(be.blank).type(user.first_name)
-            browser.element('//input[@id="lastName"]').should(be.blank).type(user.last_name)
-            browser.element('//input[@id="userEmail"]').should(be.blank).type(user.email)
+    def enter_user_details(self, user):
+        browser.element('//input[@id="firstName"]').should(be.blank).type(user.first_name)
+        browser.element('//input[@id="lastName"]').should(be.blank).type(user.last_name)
+        browser.element('//input[@id="userEmail"]').should(be.blank).type(user.email)
 
-            gender_mapping = {
-                Gender.MALE: '//input[@id="gender-radio-1"]',
-                Gender.FEMALE: '//input[@id="gender-radio-2"]',
-                Gender.OTHER: '//input[@id="gender-radio-3"]'
-            }
+        gender_mapping = {
+            Gender.MALE: '//input[@id="gender-radio-1"]',
+            Gender.FEMALE: '//input[@id="gender-radio-2"]',
+            Gender.OTHER: '//input[@id="gender-radio-3"]'
+        }
 
-            selected_gender = Gender(user.gender)
-            browser.element(gender_mapping.get(selected_gender, gender_mapping[Gender.OTHER])).double_click()
+        selected_gender = Gender(user.gender)
+        browser.element(gender_mapping.get(selected_gender, gender_mapping[Gender.OTHER])).double_click()
 
-            browser.element('//input[@id="userNumber"]').should(be.blank).type(user.phone_number)
-            browser.element('//input[@id="dateOfBirthInput"]').click()
-            browser.element('//select[@class="react-datepicker__month-select"]').click().element(
-                by.text(user.month)).click()
-            browser.element('//select[@class="react-datepicker__year-select"]').click().element(
-                by.text(user.year)).click()
-            browser.element(f'//div[@class="react-datepicker__day react-datepicker__day--0{user.day}"]').click()
-            browser.element('//input[@id="subjectsInput"]').should(be.blank).type(user.subject).press_enter()
+        browser.element('//input[@id="userNumber"]').should(be.blank).type(user.phone_number)
+        browser.element('//input[@id="dateOfBirthInput"]').click()
+        browser.element('//select[@class="react-datepicker__month-select"]').click().element(
+            by.text(user.month)).click()
+        browser.element('//select[@class="react-datepicker__year-select"]').click().element(
+            by.text(user.year)).click()
+        browser.element(f'//div[@class="react-datepicker__day react-datepicker__day--0{user.day}"]').click()
+        browser.element('//input[@id="subjectsInput"]').should(be.blank).type(user.subject).press_enter()
 
-            hobby_mapping = {
-                Hobby.SPORTS: '//label[@for="hobbies-checkbox-1"]',
-                Hobby.READING: '//label[@for="hobbies-checkbox-2"]',
-                Hobby.MUSIC: '//label[@for="hobbies-checkbox-3"]'
-            }
+        hobby_mapping = {
+            Hobby.SPORTS: '//label[@for="hobbies-checkbox-1"]',
+            Hobby.READING: '//label[@for="hobbies-checkbox-2"]',
+            Hobby.MUSIC: '//label[@for="hobbies-checkbox-3"]'
+        }
 
-            selected_hobby = Hobby(user.hobby)
-            browser.element(hobby_mapping.get(selected_hobby)).click()
+        selected_hobby = Hobby(user.hobby)
+        browser.element(hobby_mapping.get(selected_hobby)).click()
 
-            browser.element('//input[@id="uploadPicture"]').send_keys(path(user.picture))
-            browser.element('//textarea[@id="currentAddress"]').should(be.blank).type(user.address)
-            browser.element('//input[@id="react-select-3-input"]').type(user.state).press_enter()
-            browser.element('//input[@id="react-select-4-input"]').type(user.city).press_enter()
-            browser.element('//button[@id="submit"]').press_enter()
-            return self
+        browser.element('//input[@id="uploadPicture"]').send_keys(path(user.picture))
+        browser.element('//textarea[@id="currentAddress"]').should(be.blank).type(user.address)
+        browser.element('//input[@id="react-select-3-input"]').type(user.state).press_enter()
+        browser.element('//input[@id="react-select-4-input"]').type(user.city).press_enter()
+        browser.element('//button[@id="submit"]').press_enter()
+        return self
 
-    with allure.step('Submit form'):
-        def assert_submission_message(self, message):
-            browser.element('//div[@class="modal-header"]').should(have.text(message))
-            return self
+    def assert_submission_message(self, message):
+        browser.element('//div[@class="modal-header"]').should(have.text(message))
+        return self
 
-    with allure.step("Check form results"):
-        def assert_form_details(self, user):
-            browser.element('//table[@class="table table-dark table-striped table-bordered table-hover"]').should(
-                have.text(
-                    f'{user.first_name} {user.last_name}' and
-                    user.email and
-                    user.gender and
-                    user.phone_number and
-                    f'{user.day} {user.month},{user.year}' and
-                    user.subject and
-                    user.hobby and
-                    user.picture and
-                    user.address and
-                    f'{user.state} {user.city}'
-                ))
-            return self
+    def assert_form_details(self, user):
+        browser.element('//table[@class="table table-dark table-striped table-bordered table-hover"]').should(
+            have.text(
+                f'{user.first_name} {user.last_name}' and
+                user.email and
+                user.gender and
+                user.phone_number and
+                f'{user.day} {user.month},{user.year}' and
+                user.subject and
+                user.hobby and
+                user.picture and
+                user.address and
+                f'{user.state} {user.city}'
+            ))
+        return self
 
-    with allure.step('Close the form'):
-        def close_modal(self):
-            browser.element('//button[@id="closeLargeModal"]').press_enter()
-            return self
+    def close_modal(self):
+        browser.element('//button[@id="closeLargeModal"]').press_enter()
+        return self
--- tests/test_demo_qa.py
+++ tests/test_demo_qa.py
@@ -1,12 +1,19 @@
+import allure
 from demoqa_tests.pages.registation_page import RegistrationPage
 from demoqa_tests.models.user import user
 from demoqa_tests.models.constatn import SUBMISSION_MESSAGE
 
 
+@allure.title("Successful fill form")
 def test_form(browser_management):
     registration_page = RegistrationPage()
-    registration_page.open_page()
-    registration_page.enter_user_details(user)
-    registration_page.assert_submission_message(SUBMISSION_MESSAGE)
-    registration_page.assert_form_details(user)
-    registration_page.close_modal()
+    with allure.step("Open registrations form"):
+        registration_page.open_page()
+    with allure.step("Fill form"):
+        registration_page.enter_user_details(user)
+    with allure.step('Submit form'):
+        registration_page.assert_submission_message(SUBMISSION_MESSAGE)
+    with allure.step("Check form results"):
+        registration_page.assert_form_details(user)
+    with allure.step('Close the form'):
+        registration_page.close_modal()
