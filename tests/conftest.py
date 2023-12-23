7783fb2121ba048e9832b744ff66aa42d15b235d
Ilya Chukov <ilyachuk@gmail.com>
2023-12-23 18:15:37 +0300

    fixed conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,6 @@
 import pytest
-from selene import browser, Browser, Config
+from selene import browser
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
 from settings import config
 from utils import attach
 
@@ -33,11 +31,8 @@ def browser_management():
             options=driver_options
         )
 
-        # browser_selenoid = Browser(Config(driver))
         browser.config.driver = driver
-        # yield
-        #
-        # browser.quit()
+
     else:
         browser.config.driver_name = config.driver_name
 
@@ -47,29 +42,5 @@ def browser_management():
     attach.add_logs(browser)
     attach.add_html(browser)
     attach.add_video(browser)
-
-    browser.quit()
-
-
-@pytest.fixture()
-def selenoid_browser():
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": False
-        }
-    }
-
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-
-    browser = Browser(Config(driver))
-    yield browser
 
     browser.quit()
